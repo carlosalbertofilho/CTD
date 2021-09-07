c8c20c73d9b73d714b0ce443a0ae73e9e0cfbe67
carlosalbertofilho <carlosalberto_filho@outlook.com>
2021-09-07 12:32:26 -0300

    Ajuste da Mesa de Trabalho - Aula 12 - Front End 1

---
+++ ProgImperativa/aula08/exercicioIgualdade.js
@@ -0,0 +1,12 @@
+// Exercício -- Igualdade, if e else
+
+let linguagem = "javascript";
+
+if (linguagem == "javascript")
+{
+    console.log("Estou aprendendo");
+} else
+{
+    console.log("Aprenderei mais tarde");
+}
+
---
+++ ProgImperativa/aula08/meuPrimeiroIf.js
@@ -0,0 +1,11 @@
+// Exercício -- Meu primeiro if
+
+let status = true;
+
+if (status)
+{
+    console.log("O valor é true - Verdadeiro");
+} else
+{
+    console.log("O valor é false - Falso"); 
+}
