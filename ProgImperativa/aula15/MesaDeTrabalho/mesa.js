f70b9b3b134c96aee2f8f34d83b42d1174a73334
carlosalbertofilho <carlosalberto_filho@outlook.com>
2021-09-21 11:33:57 -0300

    Alterações Mesa de Trabalho - Aula 15 - Programação Imperativa

--- ProgImperativa/aula15/MesaDeTrabalho/mesa.js
+++ ProgImperativa/aula15/MesaDeTrabalho/mesa.js
@@ -3,29 +3,68 @@
     O controlador deve ser capaz de:
     ( x ) Cadastrar um novo aluno com nome, sobrenome.
     ( x ) Validar se o nome e sobrenome do aluno é uma string e/ou não esta vazio.
-    (  ) Caso passe na validação:
-        (  ) Salvar o cadastro em um arquivo json.
-    (  ) Caso o contrário:
-        (  ) Recusar o Cadastro com uma mensagem de erro.
+    ( x  ) Caso passe na validação:
+        ( x ) Salvar o cadastro em um arquivo json.
+    ( x ) Caso o contrário:
+        ( x ) Recusar o Cadastro com uma mensagem de erro.
 */
 
-function Aluno(nomeAluno, sobrenomeAluno) {
+// Importação do modulo path
+const path = require('path')
+
+/*
+ * Importação do módulo File System do NodeJs
+ */
+const fs = require('fs')
+
+// Carrega o arquivo do Banco de Dados
+const arquivo = fs.readFileSync(path.resolve('.', 'modelo.json'), 'utf-8')
+
+// Converte o arquivo de texto para objeto lieral para poder manipulá-lo.
+const arquivoConvertidoEmJSON = JSON.parse(arquivo)
+
+function Aluno (nomeAluno, sobreNomeAluno) {
   let nome
-  let sobrenome = sobrenome
-  this.setNome = function(nome){
-      if (nome != '' && typeof nome == String) this.nome = nome
+  let sobreNome
+  setNome()
+  setSobreNome()
+
+  function validaNome () {
+    return nomeAluno !== '' && typeof nomeAluno === 'string'
   }
-  this.setSobrenome = function (sobrenome)  {
-      if (sobrenome != '' && typeof sobrenome == String) sobrenome = sobrenome
+
+  function validaSobreNome () {
+    return sobreNomeAluno !== '' && typeof sobreNomeAluno === 'string'
   }
-  this.getNome = function(){
+
+  function setNome () {
+    if (validaNome()) nome = nomeAluno
+  }
+
+  function setSobreNome () {
+    if (validaSobreNome()) sobreNome = sobreNomeAluno
+  }
+
+  this.getNome = function () {
     return nome
   }
   this.getSobrenome = function () {
-    return getSobrenome
+    return sobreNome
   }
+  function salvarNaBaseDados () {
+    if (validaNome() && validaSobreNome()) {
+      arquivoConvertidoEmJSON.push({
+        nome,
+        sobreNome
+      })
+      const arquivoConvertidoEmString = JSON.stringify(arquivoConvertidoEmJSON)
+      fs.writeFileSync(path.resolve('.', 'modelo.json'), arquivoConvertidoEmString)
+    } else {
+      return 'Não é um nome válido.'
+    }
+  }
+  salvarNaBaseDados()
 }
 
-let teste2 = new Aluno('Carlos', 'Filho' )
+const teste2 = new Aluno('Carlos', 'Filho')
 console.log(teste2.getNome())
-
