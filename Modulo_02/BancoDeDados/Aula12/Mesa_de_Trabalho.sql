ca7bd6b2d55625737e732ca98118b2fc7a1bdcdf
carlosalbertofilho <carlosalberto_filho@outlook.com>
2021-12-03 21:33:21 -0300

    Aula12

---
+++ Modulo_02/BancoDeDados/Aula12/Mesa_de_Trabalho.sql
@@ -0,0 +1,26 @@
+-- Mesa de Trabalho - Aula 12
+-- Aluno: Carlos Alberto Silva dos Santos Filho
+-- Turma: 13N
+
+
+-- Projeto Spotify
+USE Spotify;
+
+-- 1. Liste as músicas que possuem a letra "z" no título.
+-- 2. Liste as músicas que têm a letra "a" como segundo caractere e a letra "s" como último caractere.
+-- 3. Mostre a lista de reprodução que contém mais músicas, renomeando as colunas, colocando em maiúscula
+-- a primeira letra, os acentos correspondentes e adicionando os espaços entre as palavras.
+-- 4. Em outro momento, obtenha uma lista com os 5 usuários mais jovens, a partir dos próximos 10 registros.
+-- 6. Liste as 5 músicas com mais reproduções, em ordem decrescente.
+-- 7. Gere um relatório de todos os álbuns em ordem alfabética.
+-- 8. Liste todos os álbuns que não possuem imagem, organizados em ordem alfabética.
+-- 9. Insira um novo usuário com os seguintes dados (leve em consideração os relacionamentos):
+-- nomeusuario: novousuariodespotify@gmail.com
+-- Nome e sobrenome: Elmer Santos 
+-- password: S4321m
+-- Data de nacimiento: 15/11/1991
+-- Sexo: Masculino
+-- Código Postal: B4129ATF
+-- País: Brasil
+-- 10.  Exclua todas as músicas do gênero "pop" da tabela generoxcancao.
+-- 11.   Edite todos os artistas que não possuem uma imagem carregada e carregue para eles o texto "Imagem em falta" na coluna de imagens.
