e374d55022092bae7fcf0c5dd97f49135a55947d
carlosalbertofilho <carlosalberto_filho@outlook.com>
2022-02-21 22:21:35 -0300

    Modulo 03 - Front End II

---
+++ Modulo_03/FrontEnd2/main.js
@@ -0,0 +1,13 @@
+/**** Grupo 06
+* Carlos Filho, Alan menezes 
+* Denis Carbone, Marina Fachinelo 
+* e Thomas Breno.
+*/
+
+const numbers = [9, 15, 24, 32, 70]
+let sum = 0
+
+for (const number of numbers)
+{
+  console.log(sum += number)
+}
